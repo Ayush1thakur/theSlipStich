a3afa13d24308a8a12708d69eb7350dea2cccd8b
Ayushmaan <118063648+ayushmaan2207@users.noreply.github.com>
2025-03-07 10:41:59 +0530

    Update permissions.js

--- models/permissions.js
+++ models/permissions.js
@@ -1,5 +1,5 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const sequelize = require('../config/sequelizeDB');
 
 const Permissions = sequelize.define('Permissions', {
         Permissionsid: {
